9f6cf3f728dfe3917fa5a0917dbd84b79b8da49b
Tim Schneider <s8tiscne@stud.uni-saarland.de>
2024-12-01 12:27:44 +0100

    Part1 solved

---
+++ src/bin/01.rs
@@ -0,0 +1,69 @@
+use anyhow::*;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use code_timing_macros::time_snippet;
+use const_format::concatcp;
+use adv_code_2024::*;
+
+const DAY: &str = "01";
+const INPUT_FILE: &str = concatcp!("input/", DAY, ".txt");
+
+const TEST: &str = "\
+3   4
+4   3
+2   5
+1   3
+3   9
+3   3
+";
+
+fn main() -> Result<()> {
+    start_day(DAY);
+
+    //region Part 1
+    println!("=== Part 1 ===");
+
+    fn part1<R: BufRead>(reader: R) -> Result<usize> {
+        // TODO: Solve Part 1 of the puzzle
+        let (mut list1, mut list2): (Vec<usize>, Vec<usize>) = reader
+            .lines()
+            .filter_map(Result::ok)
+            .filter_map(|line| {
+                let mut parts = line.split_whitespace();
+                Some((parts.next()?.parse::<usize>().unwrap(), parts.next()?.parse::<usize>().unwrap()))
+            })
+            .unzip();
+        list1.sort();
+        list2.sort();
+
+        let mut answer = 0;
+        for i in 0..list1.len() {
+           answer += list1[i].abs_diff(list2[i]);
+        }
+        Ok(answer)
+    }
+
+    // TODO: Set the expected answer for the test input
+    assert_eq!(11, part1(BufReader::new(TEST.as_bytes()))?);
+
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result = time_snippet!(part1(input_file)?);
+    println!("Result = {}", result);
+    //endregion
+
+    //region Part 2
+    println!("\n=== Part 2 ===");
+
+    fn part2<R: BufRead>(reader: R) -> Result<usize> {
+    Ok(0)
+    }
+
+    assert_eq!(0, part2(BufReader::new(TEST.as_bytes()))?);
+
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result = time_snippet!(part2(input_file)?);
+    println!("Result = {}", result);
+    //endregion
+
+    Ok(())
+}
