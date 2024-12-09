c2fa19597c8cab25e2189a4107a54067ee10a49b
Tim Schneider <s8tiscne@stud.uni-saarland.de>
2024-12-09 11:31:46 +0100

    Day 09 Part 02 done

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -86,17 +86,79 @@ fn main() -> Result<()> {
     //endregion
 
     //region Part 2
-    // println!("\n=== Part 2 ===");
-    //
-    // fn part2<R: BufRead>(reader: R) -> Result<usize> {
-    //     Ok(0)
-    // }
-    //
-    // assert_eq!(0, part2(BufReader::new(TEST.as_bytes()))?);
-    //
-    // let input_file = BufReader::new(File::open(INPUT_FILE)?);
-    // let result = time_snippet!(part2(input_file)?);
-    // println!("Result = {}", result);
+    println!("\n=== Part 2 ===");
+
+    #[derive(Debug)]
+    struct FileBlock {
+        id: usize,
+        start: usize,
+        len: usize,
+    }
+
+    #[derive(Debug)]
+    struct FreeSpace {
+        start: usize,
+        len: usize,
+    }
+    fn part2<R: BufRead>(reader: R) -> Result<usize> {
+        let input = reader.lines().flatten().collect::<String>();
+        let mut id = 0;
+        let mut pos = 0;
+        let mut free = false;
+        let mut files = vec![];
+        let mut free_space = vec![];
+        for c in input.chars() {
+            let len = c.to_digit(10).unwrap() as usize;
+            if free {
+                free_space.push(FreeSpace {
+                    start: pos,
+                    len,
+                });
+            } else {
+                files.push(FileBlock {
+                    id,
+                    start: pos,
+                    len,
+                });
+                id += 1;
+            }
+            free = !free;
+            pos += len;
+        }
+
+        for file in files.iter_mut().rev() {
+            for free in free_space.iter_mut() {
+                if free.len < file.len {
+                    continue;
+                } else if file.start < free.start {
+                    break;
+                }
+                free.len -= file.len;
+                file.start = free.start;
+                free.start += file.len;
+                break;
+            }
+        }
+
+        let answer = files
+            .iter()
+            .map(|file| -> usize {
+                let mut sum = 0;
+                for i in file.start..file.start + file.len {
+                    sum += file.id * i;
+                }
+                sum
+            })
+            .sum();
+
+        Ok(answer)
+    }
+
+    assert_eq!(2858, part2(BufReader::new(TEST.as_bytes()))?);
+
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result = time_snippet!(part2(input_file)?);
+    println!("Result = {}", result);
     //endregion
 
     Ok(())
